7ac5630b0b780c5cacc661b4580b33ca81ae588f
FrostyKitten02 <alen.fridau02@gmail.com>
2024-05-12 21:31:34 +0200

    #20 - add project id to project list

--- src/main/java/si/feri/itk/projectmanager/dto/model/ProjectDto.java
+++ src/main/java/si/feri/itk/projectmanager/dto/model/ProjectDto.java
@@ -7,12 +7,14 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @NoArgsConstructor
 public class ProjectDto {
+    private UUID id;
     private String title;
     private String ownerId;
     private LocalDate startDate;
