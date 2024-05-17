3e7dff7e88c5f009ec70750d732a1995587159a0
FrostyKitten02 <alen.fridau02@gmail.com>
2024-05-17 20:01:14 +0200

    #24 - project service refactor

--- src/main/java/si/feri/itk/projectmanager/util/RequestUtil.java
+++ src/main/java/si/feri/itk/projectmanager/util/RequestUtil.java
@@ -1,12 +1,34 @@
 package si.feri.itk.projectmanager.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import si.feri.itk.projectmanager.exceptions.implementation.BadRequestException;
+import si.feri.itk.projectmanager.paging.SortInfo;
+import si.feri.itk.projectmanager.paging.request.SortInfoRequest;
 import si.feri.itk.projectmanager.security.SecurityConstants;
 
+@Slf4j
 public class RequestUtil {
     private RequestUtil() {}
     public static String getUserId(HttpServletRequest servletRequest) {
         return (String)servletRequest.getAttribute(SecurityConstants.USER_ID);
     }
 
+    public static String getUserIdStrict(HttpServletRequest servletRequest) {
+        String userId = getUserId(servletRequest);
+        if (StringUtil.isNullOrEmpty(userId)) {
+            log.warn("Unauthorized user tried to create a project");
+            //this should never happen, we have a big problem if it does!
+            throw new BadRequestException("User is not logged in");
+        }
+        return userId;
+    }
+
+
+    public static <T extends SortInfo.IField> SortInfo<T> getSortInfoFromRequest(SortInfoRequest<T> sortInfoRequest) {
+        if (sortInfoRequest != null) {
+            return sortInfoRequest.toSortInfo();
+        }
+        return null;
+    }
 }
