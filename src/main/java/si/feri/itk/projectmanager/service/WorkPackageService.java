6801a447fe06f3e9c66b954ec00d824d8e06655e
FrostyKitten02 <alen.fridau02@gmail.com>
2024-05-20 18:26:33 +0200

    #24 - work package service refactor

--- src/main/java/si/feri/itk/projectmanager/service/WorkPackageService.java
+++ src/main/java/si/feri/itk/projectmanager/service/WorkPackageService.java
@@ -5,18 +5,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import si.feri.itk.projectmanager.dto.request.CreateWorkPackageRequest;
-import si.feri.itk.projectmanager.exceptions.implementation.BadRequestException;
 import si.feri.itk.projectmanager.exceptions.implementation.ItemNotFoundException;
 import si.feri.itk.projectmanager.model.Project;
 import si.feri.itk.projectmanager.model.WorkPackage;
 import si.feri.itk.projectmanager.repository.ProjectRepo;
 import si.feri.itk.projectmanager.repository.WorkPackageRepo;
 import si.feri.itk.projectmanager.util.RequestUtil;
-import si.feri.itk.projectmanager.util.StringUtil;
+import si.feri.itk.projectmanager.util.service.WorkPackageServiceUtil;
 
-import java.util.Optional;
 import java.util.UUID;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -24,49 +21,18 @@ public class WorkPackageService {
     private final WorkPackageRepo workPackageRepo;
     private final ProjectRepo projectRepo;
     public UUID crateWorkPackage(HttpServletRequest servletRequest, CreateWorkPackageRequest request) {
-        String userId = RequestUtil.getUserId(servletRequest);
-        if (StringUtil.isNullOrEmpty(userId)) {
-            log.warn("Unauthorized user tried to create a project");
-            //this should never happen, we have a big problem if it does!
-            throw new BadRequestException("User is not logged in");
-        }
+        String userId = RequestUtil.getUserIdStrict(servletRequest);
 
-        if (request.getProjectId() == null) {
-            throw new BadRequestException("ProjectId is required");
-        }
+        WorkPackageServiceUtil.validateCreateWorkPackageRequest(request);
+        Project project = projectRepo
+                .findByIdAndOwnerId(request.getProjectId(), userId)
+                .orElseThrow(()->new ItemNotFoundException("Project not found"));
 
-        Optional<Project> project = projectRepo.findByIdAndOwnerId(request.getProjectId(), userId);
-
-        if (project.isEmpty()) {
-            throw new ItemNotFoundException("Project not found");
-        }
-
-        if (StringUtil.isNullOrEmpty(request.getTitle())) {
-            throw new BadRequestException("Title is required");
-        }
-
-        if (request.getStartDate() == null) {
-            throw new BadRequestException("Start date is required");
-        }
-
-        if (request.getEndDate() == null) {
-            throw new BadRequestException("End date is required");
-        }
-
-        if (request.getStartDate().isAfter(request.getEndDate())) {
-            throw new BadRequestException("Start date must be before end date");
-        }
-
-        if (request.getIsRelevant() == null) {
-            throw new BadRequestException("IsRelevant is required");
-        }
-
-        WorkPackage workPackage = new WorkPackage();
-        workPackage.setTitle(request.getTitle());
-        workPackage.setStartDate(request.getStartDate());
-        workPackage.setIsRelevant(request.getIsRelevant());
-        workPackage.setEndDate(request.getEndDate());
-        workPackage.setProjectId(project.get().getId());
+        WorkPackage workPackage = WorkPackageServiceUtil.createWorkPackage(request, project.getId());
+        WorkPackageServiceUtil.validateWorkPackageDateBounds(workPackage, project);
         return workPackageRepo.save(workPackage).getId();
     }
+
+
+
 }
---
+++ src/main/java/si/feri/itk/projectmanager/util/service/WorkPackageServiceUtil.java
@@ -0,0 +1,63 @@
+package si.feri.itk.projectmanager.util.service;
+
+import si.feri.itk.projectmanager.dto.request.CreateWorkPackageRequest;
+import si.feri.itk.projectmanager.exceptions.implementation.BadRequestException;
+import si.feri.itk.projectmanager.model.Project;
+import si.feri.itk.projectmanager.model.WorkPackage;
+import si.feri.itk.projectmanager.util.StringUtil;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class WorkPackageServiceUtil {
+    private WorkPackageServiceUtil() {}
+
+    public static void validateCreateWorkPackageRequest(CreateWorkPackageRequest request) {
+        if (request.getProjectId() == null) {
+            throw new BadRequestException("ProjectId is required");
+        }
+
+        if (StringUtil.isNullOrEmpty(request.getTitle())) {
+            throw new BadRequestException("Title is required");
+        }
+
+        if (request.getStartDate() == null) {
+            throw new BadRequestException("Start date is required");
+        }
+
+        if (request.getEndDate() == null) {
+            throw new BadRequestException("End date is required");
+        }
+
+        if (request.getStartDate().isAfter(request.getEndDate())) {
+            throw new BadRequestException("Start date must be before end date");
+        }
+
+        if (request.getIsRelevant() == null) {
+            throw new BadRequestException("IsRelevant is required");
+        }
+    }
+
+    public static void validateWorkPackageDateBounds(WorkPackage wp, Project project) {
+        LocalDate taskStartDate = wp.getStartDate();
+        LocalDate taskEndDate = wp.getEndDate();
+        if (taskStartDate.isAfter(project.getEndDate()) || taskStartDate.isBefore(project.getStartDate())) {
+            throw new BadRequestException("Work package start date must be between project start and end date");
+        }
+
+        if (taskEndDate.isAfter(project.getEndDate()) || taskEndDate.isBefore(project.getStartDate())) {
+            throw new BadRequestException("Work package end date must be between project start and end date");
+        }
+    }
+
+    public static WorkPackage createWorkPackage(CreateWorkPackageRequest request, UUID projectId) {
+        WorkPackage workPackage = new WorkPackage();
+        workPackage.setTitle(request.getTitle());
+        workPackage.setStartDate(request.getStartDate());
+        workPackage.setIsRelevant(request.getIsRelevant());
+        workPackage.setEndDate(request.getEndDate());
+        workPackage.setProjectId(projectId);
+        return workPackage;
+    }
+
+}
