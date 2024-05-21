4598c65ed2e804b5c6f878bc85f6dcbabd8a3ead
FrostyKitten02 <alen.fridau02@gmail.com>
2024-05-21 17:51:12 +0200

    #26 - person type rework

--- src/main/java/si/feri/itk/projectmanager/repository/PersonHasTypeRepo.java
+++
@@ -1,9 +0,0 @@
-package si.feri.itk.projectmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import si.feri.itk.projectmanager.model.person.PersonHasType;
-
-import java.util.UUID;
-
-public interface PersonHasTypeRepo extends JpaRepository<PersonHasType, UUID> {
-}
