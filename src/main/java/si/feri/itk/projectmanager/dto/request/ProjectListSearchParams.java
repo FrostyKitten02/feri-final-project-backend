a831772a380e763ce6fe81be28a7bea09b253c59
FrostyKitten02 <alen.fridau02@gmail.com>
2024-06-22 20:09:07 +0200

    #49 - project list search start and end date limits

--- src/main/java/si/feri/itk/projectmanager/dto/request/ProjectListSearchParams.java
+++ src/main/java/si/feri/itk/projectmanager/dto/request/ProjectListSearchParams.java
@@ -3,9 +3,17 @@ package si.feri.itk.projectmanager.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class ProjectListSearchParams {
     private String searchStr;
     private Boolean searchOnlyOwnedProjects;
+
+    private LocalDate startDateFrom;
+    private LocalDate startDateTo;
+
+    private LocalDate endDateFrom;
+    private LocalDate endDateTo;
 }
