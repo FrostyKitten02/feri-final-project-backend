8a01e58c3b2019ff24bbd07adfe5f7f2f596bf4e
FrostyKitten02 <alen.fridau02@gmail.com>
2024-07-03 15:02:34 +0200

    #0000 - fixed project update schema

--- src/main/java/si/feri/itk/projectmanager/interfaces/IProjectBudgetRequest.java
+++ src/main/java/si/feri/itk/projectmanager/interfaces/IProjectBudgetRequest.java
@@ -1,8 +1,10 @@
 package si.feri.itk.projectmanager.interfaces;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public interface IProjectBudgetRequest {
+    UUID getProjectBudgetSchemaId();
     BigDecimal getStaffBudget();
     BigDecimal getTravelBudget();
     BigDecimal getEquipmentBudget();
