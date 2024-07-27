18d1a83ccf07055922dbfb0799d72ec1b833b8f6
FrostyKitten02 <alen.fridau02@gmail.com>
2024-07-27 11:04:22 +0200

    #60 - removed person on task

--- src/main/java/si/feri/itk/projectmanager/service/TaskService.java
+++ src/main/java/si/feri/itk/projectmanager/service/TaskService.java
@@ -4,23 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import si.feri.itk.projectmanager.dto.request.task.AddPersonToTaskRequest;
 import si.feri.itk.projectmanager.dto.request.task.CreateTaskRequest;
 import si.feri.itk.projectmanager.dto.request.task.UpdateTaskRequest;
 import si.feri.itk.projectmanager.exceptions.implementation.BadRequestException;
 import si.feri.itk.projectmanager.exceptions.implementation.ItemNotFoundException;
-import si.feri.itk.projectmanager.model.PersonOnTask;
 import si.feri.itk.projectmanager.model.Project;
 import si.feri.itk.projectmanager.model.Task;
 import si.feri.itk.projectmanager.model.WorkPackage;
-import si.feri.itk.projectmanager.repository.PersonOnTaskRepo;
 import si.feri.itk.projectmanager.repository.ProjectRepo;
 import si.feri.itk.projectmanager.repository.TaskRepo;
 import si.feri.itk.projectmanager.repository.WorkPackageRepo;
 import si.feri.itk.projectmanager.util.RequestUtil;
 import si.feri.itk.projectmanager.util.service.TaskServiceUtil;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -31,7 +27,6 @@ public class TaskService {
     private final TaskRepo taskRepo;
     private final ProjectRepo projectRepo;
     private final WorkPackageRepo workPackageRepo;
-    private final PersonOnTaskRepo personOnTaskRepo;
 
     public void updateTask(UUID taskId, UpdateTaskRequest updateTaskRequest, HttpServletRequest servletRequest) {
         final String userId = RequestUtil.getUserIdStrict(servletRequest);
@@ -77,25 +72,4 @@ public class TaskService {
         return workPackage.get();
     }
 
-    public void addPersonToTask(UUID taskId, UUID personId, AddPersonToTaskRequest request, HttpServletRequest servletRequest) {
-        String userId = RequestUtil.getUserIdStrict(servletRequest);
-        TaskServiceUtil.validateAddPersonToTask(taskId, personId, request);
-
-        projectRepo.findProjectByTaskIdAndOwnerId(taskId, userId).orElseThrow(() -> new BadRequestException("Invalid task id"));
-        //we will allow changing occupancy on task, but we will handle it on another endpoint
-        validatePersonNotAssignedToTask(taskId, personId);
-
-        Task task = taskRepo.findById(taskId).orElseThrow(() -> new ItemNotFoundException("Task not found"));
-        PersonOnTask personOnTask = TaskServiceUtil.createNewPersonOnTask(taskId, personId, request, task.getEndDate());
-        TaskServiceUtil.validatePersonOnTaskBounds(personOnTask, task);
-
-        personOnTaskRepo.save(personOnTask);
-    }
-
-    private void validatePersonNotAssignedToTask(UUID taskId, UUID personId) {
-        List<PersonOnTask> personOnTaskList = personOnTaskRepo.findAllByTaskIdAndPersonId(taskId, personId);
-        if (!personOnTaskList.isEmpty()) {
-            throw new BadRequestException("Person is not assigned to this task");
-        }
-    }
 }
