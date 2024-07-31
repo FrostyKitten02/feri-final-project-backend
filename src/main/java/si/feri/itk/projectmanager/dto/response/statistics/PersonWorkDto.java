086e47c83ec7dd75b428b908d9a5085c16ed4437
FrostyKitten02 <107218950+FrostyKitten02@users.noreply.github.com>
2024-07-31 12:22:46 +0200

    #65 - added person info on statistics (#69)

--- src/main/java/si/feri/itk/projectmanager/dto/response/statistics/PersonWorkDto.java
+++ src/main/java/si/feri/itk/projectmanager/dto/response/statistics/PersonWorkDto.java
@@ -2,6 +2,7 @@ package si.feri.itk.projectmanager.dto.response.statistics;
 
 import lombok.Getter;
 import lombok.Setter;
+import si.feri.itk.projectmanager.dto.model.person.PersonDto;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -9,7 +10,7 @@ import java.util.UUID;
 @Getter
 @Setter
 public class PersonWorkDto {
-    private UUID personId;
+    private PersonDto person;
     private UUID occupancyId;
     private BigDecimal totalWorkPm;
     private BigDecimal avgSalary;
