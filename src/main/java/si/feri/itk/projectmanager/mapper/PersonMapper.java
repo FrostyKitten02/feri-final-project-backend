db4ddf5498a0ca4c139ad2cc83b42d2867cb3b10
FrostyKitten02 <alen.fridau02@gmail.com>
2024-08-01 12:02:53 +0200

    #75 - person dto fix

--- src/main/java/si/feri/itk/projectmanager/mapper/PersonMapper.java
+++ src/main/java/si/feri/itk/projectmanager/mapper/PersonMapper.java
@@ -2,13 +2,14 @@ package si.feri.itk.projectmanager.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import si.feri.itk.projectmanager.dto.model.person.PersonDtoImpl;
+import si.feri.itk.projectmanager.dto.model.person.PersonDto;
+import si.feri.itk.projectmanager.dto.model.person.PersonListDto;
 import si.feri.itk.projectmanager.model.person.Person;
 import si.feri.itk.projectmanager.model.person.PersonList;
 
 @Mapper
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
-    PersonDtoImpl toDto(Person person);
-    PersonDtoImpl toDto(PersonList personList);
+    PersonDto toDto(Person person);
+    PersonListDto toDto(PersonList personList);
 }
