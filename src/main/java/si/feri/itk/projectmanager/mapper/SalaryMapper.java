9cc886f139ddfa6ecf21aa142b1d087c8f13bfc6
FrostyKitten02 <alen.fridau02@gmail.com>
2024-08-01 12:06:39 +0200

    #75 - salary dto fix

--- src/main/java/si/feri/itk/projectmanager/mapper/SalaryMapper.java
+++ src/main/java/si/feri/itk/projectmanager/mapper/SalaryMapper.java
@@ -2,14 +2,14 @@ package si.feri.itk.projectmanager.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import si.feri.itk.projectmanager.dto.model.salary.SalaryDtoImpl;
+import si.feri.itk.projectmanager.dto.model.salary.SalaryDto;
+import si.feri.itk.projectmanager.dto.model.salary.SalaryListDto;
 import si.feri.itk.projectmanager.model.person.Salary;
 import si.feri.itk.projectmanager.model.person.SalaryList;
 
 @Mapper
 public interface SalaryMapper {
     SalaryMapper INSTANCE = Mappers.getMapper(SalaryMapper.class);
-
-    SalaryDtoImpl toDto(Salary salary);
-    SalaryDtoImpl toDto(SalaryList salary);
+    SalaryDto toDto(Salary salary);
+    SalaryListDto toDto(SalaryList salary);
 }
